e602709cf071c156a7e629d36a7ef38494f69d67
Ashwin Prakash <ashp94@gmail.com>
2018-12-06 18:02:45 -0600

    Completed exercise displaying foods

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -8,21 +8,27 @@ const p = (title, style) => {
 }
 
 const foodlist = (name, type, ethnicity) => `
- <div class="foodlist">
+ <div id="foodlists">
     ${h1(name, "foodname")}
     ${p(type, "foodtype")}  
     ${p(ethnicity, "foodethnicity")}
  </div>
 `
 
-
-
 fetch("http://localhost:8088/food/")
-  .then(foods => foods.json())
+  .then(foods => foods.json())  
   .then(parsedFoods => {
-    parsedFoods.forEach(function(allFoods){
-        foodcontainer.innerHTML += foodlist(allFoods.name, allFoods.type, allFoods.ethnicity);
-        console.table(allFoods)
-    }
-    )
-})
+   parsedFoods.forEach(allFoods=> {
+    foodcontainer.innerHTML += foodlist(allFoods.name, allFoods.type, allFoods.ethnicity)
+            })
+        })
+
+
+// fetch(`https://world.openfoodfacts.org/api/v0/product/${foods.barcode}.json`)
+            // .then(foods => foods.json())
+            // .then(productInfo => {
+            //allFoods.igredients = productinfo.product.ingredients_text
+            //allFoods.country = productinfo.product.countries
+            //allFoods.sugar = productinfo.product.nutriments.sugear
+            //allFoods.fat = productinfo.product.nutriments.fat
+            //allFoods.calories = productinfo.product.nutriments.energy_serving
