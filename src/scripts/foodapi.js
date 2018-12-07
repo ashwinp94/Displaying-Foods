fc747e52409bb907fce98caa922f34eceb502b2a
Ashwin Prakash <ashp94@gmail.com>
2018-12-06 18:10:51 -0600

    Mostly done, still needs styling changes, but information is being displayed

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -7,11 +7,16 @@ const p = (title, style) => {
     return `<p class="${style}">${title}</p>`
 }
 
-const foodlist = (name, type, ethnicity) => `
+const foodlist = (name, type, ethnicity, ingredients, country, sugar, fat, calories) => `
  <div id="foodlists">
     ${h1(name, "foodname")}
     ${p(type, "foodtype")}  
     ${p(ethnicity, "foodethnicity")}
+    ${p(ingredients, "fooding")}
+    ${p(country, "foodcountry")}
+    ${p(sugar, "foodsugar")}
+    ${p(fat, "foodfat")}
+    ${p(calories, "foodcalories")}
  </div>
 `
 
@@ -19,9 +24,18 @@ fetch("http://localhost:8088/food/")
   .then(foods => foods.json())  
   .then(parsedFoods => {
    parsedFoods.forEach(allFoods=> {
-    foodcontainer.innerHTML += foodlist(allFoods.name, allFoods.type, allFoods.ethnicity)
-            })
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${allFoods.barcode}.json`)
+            .then(foods => foods.json())
+            .then(productinfo => {
+            allFoods.ingredients = productinfo.product.ingredients_text;
+            allFoods.country = "Country: " + productinfo.product.countries;
+            allFoods.sugar = productinfo.product.nutriments.sugar;
+            allFoods.fat = productinfo.product.nutriments.fat;
+            allFoods.calories = productinfo.product.nutriments.energy_serving;     
+            foodcontainer.innerHTML += foodlist(allFoods.name, allFoods.type, allFoods.ethnicity, allFoods.ingredients, allFoods.country, allFoods.sugar, allFoods.fat, allFoods.calories); 
         })
+    })
+})
 
 
 // fetch(`https://world.openfoodfacts.org/api/v0/product/${foods.barcode}.json`)
